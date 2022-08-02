69e60829b3268168c19f9212b246a4989bc4d012
Flavius <GitHub@FlavCreations.com>
2022-08-02 10:12:54 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,9 +17,7 @@ namespace BlazLink
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
             await builder.Build().RunAsync();
         }
     }
